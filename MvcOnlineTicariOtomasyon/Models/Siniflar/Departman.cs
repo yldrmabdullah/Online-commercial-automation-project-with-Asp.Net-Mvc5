124f8ff6141c59b74be263bad0b92589fb53d623
Ahmet Abdullah Yıldırım <81380051+yldrmabdullah@users.noreply.github.com>
2022-05-22 13:53:51 +0300

    Online Commercial Automation
    Online Commercial Automation project in which personnel, product, accountant, sales and invoices of sales are checked.

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Departman.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Departman
+    {
+        [Key]
+        public int Departmanid { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string DepartmanAd { get; set; }
+        public bool Durum { get; set; }
+        public ICollection<Personel> Personels { get; set; }
+       
+
+
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Urun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Urun
+    {
+        [Key]
+        public int Urunid { get; set; }
+
+        [Column(TypeName = "Varchar") ]
+        [StringLength(30)]
+        public string UrunAd { get; set; }
+       
+        
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string Marka { get; set; }
+        public short Stok { get; set; }
+        public decimal AlisFiyat { get; set; }
+        public decimal SatisFiyat { get; set; }
+        public bool Durum { get; set; }
+
+        public int Kategoriid { get; set; }
+        public virtual Kategori Kategori { get; set; }
+
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(250)]
+        public string UrunGorsel { get; set; }
+
+
+        public ICollection<SatisHareket> SatisHarekets { get; set; }
+
+
+
+
+    }
+}
